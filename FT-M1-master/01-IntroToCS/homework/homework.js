e5431ed239dc3e44df7de75aa44fca50b5f20af0
Valeabril11 <valeria.molina@live.com>
2022-06-19 23:19:04 -0300

    terminado

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,12 +1,31 @@
 'use strict'
 
 function BinarioADecimal(num) {
+//num=[1,0,1,0,1,1,1,0,0,1]
+var suma = 0;
+num = num.toString().split("").reverse().join("")
+for (let item in num){
+  if(num[item] !=0){
+    suma += Math.pow(2,item);
+  }}
+return suma;
   // tu codigo aca
-
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+let result = []
+do {
+  if (num%2 == 0){
+    num = num/2;
+    result.unshift(0);
+  }else{
+    result.unshift(1);
+    num = Math.floor(num/2);
+  }
+} while (num >= 1);
+return result.join("")
+//  return parseInt(a,2);
 
 }
 
