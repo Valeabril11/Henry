20ce03f1170706a7a6ae11ec1e085eb22377e2c5
Valeabril11 <valeria.molina@live.com>
2022-07-15 00:09:35 -0300

    TERMINADO

--- FT-M1-master/05-EstructuraDeDatos-II/homework/homework.js
+++ FT-M1-master/05-EstructuraDeDatos-II/homework/homework.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { listenerCount } = require("@11ty/eleventy/src/Util/AsyncEventEmitter");
+
 /*
 Implementar la clase LinkedList, definiendo los siguientes métodos:
   - add: agrega un nuevo nodo al final de la lista;
@@ -10,10 +12,66 @@ Implementar la clase LinkedList, definiendo los siguientes métodos:
   search(isEven), donde isEven es una función que retorna true cuando recibe por parámetro un número par, busca un nodo cuyo valor sea un número par.
   En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
+//push
 
-function LinkedList() {}
-
-function Node(value) {}
+function LinkedList() {
+  this.head=null;
+  this._length=0;
+  }
+  function Node(value) {
+    this.value = value;
+    this.next = null;
+  }
+  //let list= new LinkedList();
+  LinkedList.prototype.add = function(data){
+  let node= new Node(data);
+  let current = this.head;
+    
+   if(this.head === null){
+    this.head= node
+    this._length++;
+    return node
+  }
+  while (current.next){
+    current= current.next;
+  }
+  current.next =node;
+  this._length++;
+  return node
+  }
+  LinkedList.prototype.remove = function(){
+    
+    var current=this.head;
+    if(this._length===0)return null;
+    if(this._length===1){
+      let aux = current.value;
+      this.head=null;
+      this._length--;
+      return aux;
+    }
+    while(current.next.next){
+      current=current.next;
+    }
+    let borrado = current.next.value;
+    current.next = null;
+    this._length--;
+    console.log(borrado)
+    return borrado;
+    };
+  LinkedList.prototype.search = function(value){
+    let current=this.head 
+    if(this.head === null ) return null
+    while (current){
+     if(current.value === value) return value;
+     if (typeof value === 'function'){
+       if(value(current.value)){
+        return current.value
+       }
+     }
+     current=current.next
+        }
+    return null
+  }
 
 /*
 Implementar la clase HashTable.
@@ -30,7 +88,80 @@ La clase debe tener los siguientes métodos:
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
 
-function HashTable() {}
+// function HashTable() {
+//  this.buckets = [];
+//  this.numBuckets = 35;
+// }
+
+// HashTable.prototype.hash = function (input){
+//   let a = 0
+//   if(input.length > 0){
+
+//     for( var i = 0; i < input.length ; i++){
+//       a += input.charCodeAt(i);
+//     }
+//   }
+//   return a%this.numBuckets;
+// }
+
+
+
+// HashTable.prototype.set = function (clave , valor){
+//   if(typeof clave != "string") throw new TypeError ('Keys must be strings');
+  
+//   let b = this.hash(clave)
+//   if (this.buckets[b]=== undefined){
+//     this.buckets[b] = {};
+//   }
+//   this.buckets[b][clave] = valor;
+// }
+
+// HashTable.prototype.get = function (clave){
+//   let b = this.hash(clave);
+//   if(this.buckets[b] !== undefined) return this.buckets[b][valor]
+// }
+ 
+// HashTable.prototype.get= function(key){
+//   let i = this.hash(key);
+//   if(this.buckets[i] !== undefined) return this.buckets[i][key]
+//   }
+
+function HashTable() {
+  this.numBuckets = 35;
+  this.buckets = []
+  }
+  HashTable.prototype.hash = function(input){
+    let a=0
+  if (input.length>0){
+    for(var i=0; i<input.length;i++){
+      a+=input.charCodeAt(i)
+    }
+  }
+  return a%this.numBuckets;
+  }
+  HashTable.prototype.set= function(key,value){
+    if(typeof key != "string") throw new TypeError ('Keys must be strings')
+   let i=this.hash(key)
+    if(this.buckets[i]=== undefined){
+  this.buckets[i]={};
+    }
+    this.buckets[i][key]= value
+  }
+  HashTable.prototype.get= function(key){
+  let i = this.hash(key);
+  if(this.buckets[i] !== undefined) return this.buckets[i][key]
+  }
+  HashTable.prototype.hasKey= function(key){
+    let i = this.hash(key);
+  return this.buckets[i].hasOwnProperty(key) ? true:false 
+  }
+  
+// te amo 
+//dklasdnmkda
+// HashTable.prototype.hasKey= function(key){
+//   let i = this.hash(key);
+// return this.buckets[i].hasOwnProperty(key) ? true:false 
+// }
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
