f8df769144812e8d77c21857ff8afdf56a683c56
Valeabril11 <valeria.molina@live.com>
2022-07-15 00:26:03 -0300

    TERMINADO

--- FT-M1-master/06-EstructuraDeDatos-III/homework/homework.js
+++ FT-M1-master/06-EstructuraDeDatos-III/homework/homework.js
@@ -10,24 +10,70 @@
 
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
-
-function BinarySearchTree(data) {
-  this.data = data;
-  this.left = null;
-  this.right = null;
+function BinarySearchTree(value) {
+  this.value=value;
+  this.right=null;
+  this.left=null;
 }
-// let bst = new BinarySearchTree(data);
 
-BinarySearchTree.prototype.insert = function (data){
-if(data > this.data){
-  if(this.right === null) this.right = new BinarySearchTree(data);
-  else {this.right.insert(data)};
-}else{
-  if(data < this.data){
-    if(this.left === null) this.left = new BinarySearchTree(data);
-    else {this.left.insert(data)};
+
+BinarySearchTree.prototype.insert = function (value){
+  if (this.value > value) {
+    if(this.left === null) this.left = new BinarySearchTree(value)
+    else this.left.insert(value);
+
+  }else  if(this.value < value){
+    if(this.right === null)this.right = new BinarySearchTree(value)
+    else this.right.insert(value)
   }
+
 }
+
+BinarySearchTree.prototype.size = function (){
+   if (this.left === null && this.right === null)return 1
+   if (this.left === null && this.right !== null)return 1 + this.right.size()
+   if (this.left !== null && this.right === null)return 1 + this.left.size()
+   if (this.left !== null && this.right !== null)return 1 + this.left.size() + this.right.size()
+}
+BinarySearchTree.prototype.contains = function (value){
+
+  if (this.value === value) return true
+  if(value  > this.value){
+    if(this.right === null) return false;
+    else return this.right.contains(value)
+  }
+  if(value < this.value){
+    if(this.left === null) return false;
+    else return this.left.contains(value)
+  }}
+
+
+BinarySearchTree.prototype.depthFirstForEach = function (cb, orden){  
+	 if(orden === 'pre-order'){
+// root - izq - der
+cb(this.value)
+if(this.left !== null) this.left.depthFirstForEach(cb,orden)
+if(this.right !== null) this.right.depthFirstForEach(cb,orden)
+}else if (orden === 'post-order'){
+  //izq - der - root
+  if(this.left !== null)this.left.depthFirstForEach(cb,orden)
+  if(this.right !== null) this.right.depthFirstForEach(cb,orden)
+  cb(this.value)
+
+}else {
+// in-order   izq - root - der 
+if(this.left !== null ) this.left.depthFirstForEach(cb,orden)
+cb(this.value)
+if(this.right !== null) this.right.depthFirstForEach(cb,orden)
+}
+
+}
+BinarySearchTree.prototype.breadthFirstForEach = function (cb, array=[]){
+    if (this.left !== null) array.push(this.left)
+    if (this.right !== null) array.push(this.right)
+    cb (this.value)
+
+    if(array.length >0 )array.shift().breadthFirstForEach(cb,array)
 }
 
 // No modifiquen nada debajo de esta linea
